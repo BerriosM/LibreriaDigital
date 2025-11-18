085a4018788c1012285ea7a67bafc20ee7bedf00
BerriosM <matias.berrios13@inacapmail.cl>
2025-11-18 14:28:38 -0300

    .

--- LibreriaDigital/urls.py
+++ LibreriaDigital/urls.py
@@ -22,5 +22,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/",include("django.contrib.auth.urls")), #Todas las url de autenticacion
-    path("", TemplateView.as_view(template_name="index.html"), name="home")
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
 ]
